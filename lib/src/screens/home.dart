5b5c70df269f17866de93b37cf25dd7d552d1adc
Obinna Igwe <igwenus619@gmail.com>
2019-04-22 15:58:04 +0100

    Initial commit

---
+++ lib/src/screens/home.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  HomeScreen({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Home',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/src/widgets/theme.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MarvelAppTheme {
+
+static final Color marvelRed = Color(0xfff30e0d);
+
+  ThemeData getAppTheme(){
+    final ThemeData baseTheme = ThemeData.light();
+    return baseTheme.copyWith(
+
+    );
+  }
+
+
+}
