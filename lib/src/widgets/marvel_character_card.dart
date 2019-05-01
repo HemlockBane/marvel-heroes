9ac7aaf703e3554fc71fb1fc302691c5e1944fa0
Obinna Igwe <igwenus619@gmail.com>
2019-05-01 14:42:34 +0100

    Format code

--- lib/src/widgets/marvel_character_card.dart
+++ lib/src/widgets/marvel_character_card.dart
@@ -11,48 +11,61 @@ class _MarvelCharacterCardState extends State<MarvelCharacterCard> {
     return Container(
       height: 200,
       decoration: BoxDecoration(
-          boxShadow: [
-            BoxShadow(
-                blurRadius: 70,
-                spreadRadius: -80,
-                offset: Offset(0, 40),
-            ),
-          ],
+        boxShadow: [
+          BoxShadow(
+            blurRadius: 70,
+            spreadRadius: -80,
+            offset: Offset(0, 40),
+          ),
+        ],
       ),
       child: Card(
         elevation: 16,
         shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.horizontal(left: Radius.circular(30))
-        ),
+            borderRadius: BorderRadius.horizontal(left: Radius.circular(30))),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.horizontal(
-                    left: Radius.circular(30),
-                ),
-                image: DecorationImage(
-                    image: AssetImage('assets/images/download.jpeg'),
-                  fit: BoxFit.cover
-                )
-              ),
-              width: MediaQuery.of(context).size.width / 2.1,
-
-            ),
+            _buildImage(context),
             Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text('Wolverine'),
-                  Text('James Howlett'),
-                  Text('Is a fictional character appearing in American comic books published by Marvel'),
-                ],
+              child: Container(
+                margin: EdgeInsets.symmetric(horizontal: 16),
+                child: Column(
+                  //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text('Wolverine', style: TextStyle(fontSize: 24),),
+                    Text('James Howlett'),
+                    Expanded(
+                      child: Text('Is a fictional character appearing in American comic books published by Marvel',
+                      maxLines: 4,
+                      overflow: TextOverflow.ellipsis,),
+                    ),
+                    ListTile(
+                      contentPadding: EdgeInsets.symmetric(horizontal: 0),
+                      leading: Text('More info'),
+                      trailing: Icon(Icons.chevron_right),)
+                  ],
+                ),
               ),
             )
           ],
         ),
       ),
+    );
+  }
+
+  Widget _buildImage(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.horizontal(
+          left: Radius.circular(30),
+        ),
+        image: DecorationImage(
+            image: AssetImage('assets/images/download.jpeg'),
+            fit: BoxFit.cover),
+      ),
+      width: MediaQuery.of(context).size.width / 2.1,
     );
   }
 }
--- lib/src/widgets/theme.dart
+++ lib/src/widgets/theme.dart
@@ -7,7 +7,8 @@ static final Color marvelRed = Color(0xfff30e0d);
   ThemeData getAppTheme(){
     final ThemeData baseTheme = ThemeData.light();
     return baseTheme.copyWith(
-
+      textTheme: TextTheme(
+      )
     );
   }
 
